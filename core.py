ed3ca5e2369af55218e71b6ae14d74c030cf10ca
watersb1323 <watersb1323@gmail.com>
2017-03-28 00:03:09 +0100

    Added weighted average for normalised scoring

--- core.py
+++ core.py
@@ -9,12 +9,15 @@ import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
 
+def triangular(n):
+    return sum(range(n+1))
+
 # Define file variables
 weekly_file = 'Squash_results.xlsx'
 squash_path = 'C:\\Users\\beano\\Google Drive\\Squash\\'
 
 # Define switches
-week_num = 3
+week_num = 4
 master = True
 
 # Define columns for statistics
@@ -35,19 +38,24 @@ stats_columms = ['Name',\
 if master:
     # Read in all weeks results
     for week in range(1,week_num+1):
-        tmp_df = pd.read_excel(squash_path + weekly_file, sheetname='Week{0}_games'.format(week))
+        tmp_games_df = pd.read_excel(squash_path + weekly_file, sheetname='Week{0}_games'.format(week))
+        tmp_tables_df = pd.read_excel(squash_path + weekly_file, sheetname='Week{0}_table'.format(week))
         
         if week == 1:
-            weekly_scores_df = tmp_df
+            weekly_games_df = tmp_games_df
+            tmp_tables_df['week'] = week
+            weekly_tables_df = tmp_tables_df
         else:
-            weekly_scores_df = weekly_scores_df.append(tmp_df)
-            print(weekly_scores_df)
+            weekly_games_df = weekly_games_df.append(tmp_games_df)
+            tmp_tables_df['week'] = week
+            weekly_tables_df = weekly_tables_df.append(tmp_tables_df)
+            #print(weekly_games_df)
 else:
     # Read in weekly tab
-    weekly_scores_df = pd.read_excel(squash_path + weekly_file, sheetname='Week{0}_games'.format(week_num))
+    weekly_games_df = pd.read_excel(squash_path + weekly_file, sheetname='Week{0}_games'.format(week_num))
 
 # Obtain list of players
-players_tmp = weekly_scores_df['Player 1'].unique().tolist() + weekly_scores_df['Player 2'].unique().tolist()
+players_tmp = weekly_games_df['Player 1'].unique().tolist() + weekly_games_df['Player 2'].unique().tolist()
 players = list(set(players_tmp))
 
 weekly_stats_df = pd.DataFrame(columns=stats_columms)
@@ -55,14 +63,17 @@ weekly_stats_df = pd.DataFrame(columns=stats_columms)
 # Go through each player and retrieve their results
 for player in players:
     # Determine games player participated in
-    player_games_df = weekly_scores_df.loc[(weekly_scores_df['Player 1'] == player) | (weekly_scores_df['Player 2'] == player)]
+    player_games_df = weekly_games_df.loc[(weekly_games_df['Player 1'] == player) | (weekly_games_df['Player 2'] == player)]
     
     # Determine games won by player
-    player_wins_df = weekly_scores_df.loc[((weekly_scores_df['Player 1'] == player) &\
-                                            (weekly_scores_df['Score 1'] > weekly_scores_df['Score 2']))\
+    player_wins_df = weekly_games_df.loc[((weekly_games_df['Player 1'] == player) &\
+                                            (weekly_games_df['Score 1'] > weekly_games_df['Score 2']))\
                                             |
-                                            ((weekly_scores_df['Player 2'] == player) &\
-                                            (weekly_scores_df['Score 2'] > weekly_scores_df['Score 1']))]
+                                            ((weekly_games_df['Player 2'] == player) &\
+                                            (weekly_games_df['Score 2'] > weekly_games_df['Score 1']))]
+    
+    # Obtain table of players weekly results
+    player_tables_df = weekly_tables_df.loc[(weekly_tables_df['Name'] == player)]
     
     # Obtain points for and points against
     PF = 0
@@ -95,7 +106,18 @@ for player in players:
     DIFF = PF - PA
     BP = 0
     Points = W*3 + WB + LB
-    normalised_score = int((Points/PL)*(100.0/4.0))
+    
+    # Determine whether to use normalised score (for weekly table) or weighted score (master table)
+    normalised_score = 0
+    if master == True:
+        for _, weekly_result in player_tables_df.iterrows():
+            wk = weekly_result['week']
+            n_score = weekly_result['Normalised Score']            
+            normalised_score += int(n_score*wk/triangular(week_num))
+    else:
+        normalised_score = int((Points/PL)*(100.0/4.0))
+    
+    # Create row for player data to be added to the dataframe    
     player_data = [player, PL, W, L, PF, PA, DIFF, WB, LB, Points, normalised_score]
     
     #Create dataframe for addition to main dataframe
